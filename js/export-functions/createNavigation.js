d6fb107d66080318cd7aa0e04ad6d6a4e03cd718
Vegard M <vm932648@gmail.com>
2023-03-06 11:35:41 +0100

    Working on page layout
    Working on page layout as a scaffolding to build the page itself on. Will populate it with placeholder content and eventually content from WordPress REST API

---
+++ js/export-functions/createNavigation.js
@@ -0,0 +1,15 @@
+import { navItems } from "../export-objects/export-objects.js";
+
+const navDOM = document.querySelector(".nav_ul");
+
+function createNavigation(container, items) {
+  for (let i = 0; i < items.length; i++) {
+    container.innerHTML += `
+          <li class="nav_li">
+          <a href="${items[i].url}">${items[i].title}</a>
+          </li>
+    `;
+  }
+};
+
+createNavigation(navDOM, navItems);
---
+++ js/export-objects/export-objects.js
@@ -0,0 +1,19 @@
+// Nav items:
+export const navItems = [{
+    title: "Home",
+    url: "index.html",
+    index: 0
+},{
+    title: "Blog overview" ,
+    url: "",
+    index: 1
+},{
+    title: "About us" ,
+    url: "" ,
+    index: 2
+},{
+    title: "Contact us",
+    url: "",
+    index: 3
+}];
+
