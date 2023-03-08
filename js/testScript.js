1bb133eff8bd3bb979a7d203f4aa007c011bd03e
Vegard M <vm932648@gmail.com>
2023-03-08 13:32:31 +0100

    Working on JavaScript for HTML list creation
    Working on JavaScript for HTML list creation + splitting my main CSS and the index-spesific CSS to keep things easier to manage

---
+++ js/testScript.js
@@ -0,0 +1,39 @@
+const domElement = document.querySelector(".newest-posts");
+let apiUrl = "https://sellmo.no/Flower_Power/wp-json/wp/v2/posts";
+//I'll use the following to get a set amount of results via the URL;
+//https://sellmo.no/Flower_Power/wp-json/wp/v2/posts?per_page=4&page=1
+//This returns 4 posts per page and sets what page I want to see.
+//I can then use buttons and a "count" variable to change what page I'm on
+//Something like;
+//let count = 1;
+//button.onclick = count++
+//otherButton.onclick = count--
+//https://sellmo.no/Flower_Power/wp-json/wp/v2/posts?per_page=4&page=${count}
+
+//However, I do want the function to be usable on both my index page 
+//And the overview page. Should be doable if the "per page" is also a variable
+//Maybe something I could fix via an if statment:
+//if(container.classlist ==== "newest-posts"){per_page=4} or something
+
+function createHTML(container, array) {
+    for (let i = 0; i < array.length; i++) {
+        const singlePost = array[i];
+        container.innerHTML += `
+        <div class="single-post-in-list">
+        <h3> ${array[i].title.rendered}</h3>
+        ${array[i].excerpt.rendered}
+        <p>Published the ${array[i].date} and it is post number${i+1}</p>
+        </div>
+        `;
+    }   
+}
+
+async function getArray(container, url) {
+    let response = await fetch(url);
+    let finishedResponse = await response.json();
+    console.log(finishedResponse);
+
+    createHTML(container, finishedResponse);
+};
+
+getArray(domElement, apiUrl);
