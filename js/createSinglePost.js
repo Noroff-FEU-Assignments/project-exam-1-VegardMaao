4067f0341c75c110cba858b75322a6ff385fc251
Vegard M <vm932648@gmail.com>
2023-03-20 14:03:14 +0100

    Made a simple single post script
    Made a simple single post script which seems to work quite nicely.
    
    Still a lot of CSS'ing to do, but we're getting there. I also need to create a comments section, but that comes later on. For now I'll focus on making the must-haves as good as possible.

---
+++ js/createSinglePost.js
@@ -0,0 +1,30 @@
+const container = document.querySelector(".single-post");
+const queryString = document.location.search;
+const parameter = new URLSearchParams(queryString);
+const id = parameter.get("id");
+const apiUrl = `https://sellmo.no/Flower_Power/wp-json/wp/v2/posts/${id}?_embed`;
+
+async function createPost(url) {
+    const productResponse = await fetch(url);
+    const postInfo = await productResponse.json();
+
+    document.title = postInfo.title.rendered + "- Loppas Big Blog";
+
+    if (postInfo._embedded['wp:featuredmedia']) {
+        container.innerHTML = `
+            <img class="featured-picture" src="${postInfo._embedded['wp:featuredmedia']['0'].source_url}" alt="${postInfo._embedded['wp:featuredmedia']['0'].title.rendered}" title="${postInfo._embedded['wp:featuredmedia']['0'].title.rendered}">
+            <h1>${postInfo.title.rendered}</h1>
+            ${postInfo.content.rendered}
+            `;
+    } else {
+        container.innerHTML = `
+            <h1>${postInfo.title.rendered}</h1>
+            ${postInfo.content.rendered}
+            `;
+    }
+
+}
+
+createPost(apiUrl);
+
+//<img class="intro-picture" src="${postInfo._embedded['wp:featuredmedia']['0'].source_url}" alt="" title="">
