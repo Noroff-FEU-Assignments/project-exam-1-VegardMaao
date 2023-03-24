31bc8e29c489924fd1b408c1738afd83235b1ca9
Vegard M <vm932648@gmail.com>
2023-03-24 15:07:18 +0100

    ✨Added the post date to single posts
    Added the post date to single posts.  Had  to slice away the hour, but it wasn't too hard

--- js/export-functions/createSinglePost.js
+++ js/export-functions/createSinglePost.js
@@ -4,17 +4,23 @@ export async function createPost(container, url) {
 
     if (postInfo._embedded) {
         document.title = postInfo.title.rendered + "- Loppas Big Blog";
-
+        
         if (postInfo._embedded['wp:featuredmedia']) {
+            let date = postInfo.date;
+            let slicedDate = date.slice(0, date.length -9);
+
             container.innerHTML = `
             <img class="featured-picture" src="${postInfo._embedded['wp:featuredmedia']['0'].source_url}" alt="${postInfo._embedded['wp:featuredmedia']['0'].title.rendered}" title="${postInfo._embedded['wp:featuredmedia']['0'].title.rendered}">
             <h1>${postInfo.title.rendered}</h1>
             ${postInfo.content.rendered}
+
+            <p class="date">Posted the ${slicedDate}</p>
             `;
         } else {
             container.innerHTML = `
             <h1>${postInfo.title.rendered}</h1>
             ${postInfo.content.rendered}
+            <p class="date">Posted the ${slicedDate}</p>
             `;
         }
     }  else {
