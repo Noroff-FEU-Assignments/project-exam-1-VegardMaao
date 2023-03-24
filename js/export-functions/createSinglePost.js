d1b5579c7aa21a7d0030f40b76889e4d35318cfb
Vegard M <vm932648@gmail.com>
2023-03-24 15:14:01 +0100

    added edit dates to single posts

--- js/export-functions/createSinglePost.js
+++ js/export-functions/createSinglePost.js
@@ -6,21 +6,23 @@ export async function createPost(container, url) {
         document.title = postInfo.title.rendered + "- Loppas Big Blog";
         
         if (postInfo._embedded['wp:featuredmedia']) {
-            let date = postInfo.date;
-            let slicedDate = date.slice(0, date.length -9);
+            let postedDate = postInfo.date;
+            let slicedPostDate = postedDate.slice(0, postedDate.length -9);
+            let editDate = postInfo.modified;
+            let slicedEditDate = editDate.slice(0, editDate.length - 9);
 
             container.innerHTML = `
             <img class="featured-picture" src="${postInfo._embedded['wp:featuredmedia']['0'].source_url}" alt="${postInfo._embedded['wp:featuredmedia']['0'].title.rendered}" title="${postInfo._embedded['wp:featuredmedia']['0'].title.rendered}">
             <h1>${postInfo.title.rendered}</h1>
             ${postInfo.content.rendered}
 
-            <p class="date">Posted the ${slicedDate}</p>
+            <p class="date">Posted the ${slicedPostDate}, edited ${slicedEditDate}</p>
             `;
         } else {
             container.innerHTML = `
             <h1>${postInfo.title.rendered}</h1>
             ${postInfo.content.rendered}
-            <p class="date">Posted the ${slicedDate}</p>
+            <p class="date">Posted the ${slicedDate}, edited the </p>
             `;
         }
     }  else {
