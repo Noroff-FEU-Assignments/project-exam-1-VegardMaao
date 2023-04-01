8a36a577b25ba692ada4cc96c0c88380f96db91f
Vegard M <vm932648@gmail.com>
2023-04-01 09:49:26 +0200

    Made footer dynamic
    Made footer dynamic based on a javascript object. Easier to update

--- js/createNavigation.js
+++ js/createNavigation.js
@@ -1,11 +1,15 @@
 import { navItems } from "./export-objects/export-navigation.js";
 
+import { footerItems } from "./export-objects/export-footer.js";
+
 const navDOM = document.querySelector(".nav_ul");
+
+const footerDOM = document.querySelector(".cs_ul");
 
 function createNavigation(container, items) {
   for (let i = 0; i < items.length; i++) {
     container.innerHTML += `
-          <li class="nav_li">
+          <li>
           <a href="${items[i].url}">${items[i].title}</a>
           </li>
     `;
@@ -13,3 +17,5 @@ function createNavigation(container, items) {
 };
 
 createNavigation(navDOM, navItems);
+
+createNavigation(footerDOM, footerItems);
---
+++ js/export-objects/export-footer.js
@@ -0,0 +1,10 @@
+// Footer items:
+export const footerItems = [{
+    title: "About",
+    url: "about.html",
+    index: 0
+}, {
+    title: "Contact",
+    url: "contact.html",
+    index: 1
+}];
