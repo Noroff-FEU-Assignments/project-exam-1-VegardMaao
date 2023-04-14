391c32379e560e141d47203fde500af98d8b64a7
Vegard M <vm932648@gmail.com>
2023-04-14 17:20:31 +0200

    Removed unneded for loop
    Talked to a colleague who realized that the nested for loop was unnecessary. He’s right! There’s always an equal number of keys and values, so the [x] variable is good for both.

--- js/export-functions/submitForm.js
+++ js/export-functions/submitForm.js
@@ -6,17 +6,12 @@ export async function submitForm(e, inputs, url) {
     let dataObject = {};
     for (let i = 0; i < inputs.length; i++) {
         const singleInput = inputs[i];
-        //console.log(inputs[i]);
         arrayOfKeys.push(singleInput.name);
         arrayOfInputValues.push(singleInput.value);
     }
 
     for (let x = 0; x < arrayOfKeys.length; x++) {
-        console.log(arrayOfKeys[x]);
-        for (let y = 0; y < arrayOfInputValues.length; y++) {
-            dataObject[arrayOfKeys[x]] = arrayOfInputValues[x];
-        }
-
+        dataObject[arrayOfKeys[x]] = arrayOfInputValues[x];
     }
     
     console.log("array of keys", arrayOfKeys);
