0762b3b8c97949ca425a43b5ecb7ab9cd8edc857
Vegard M <vm932648@gmail.com>
2023-04-15 14:34:51 +0200

    Update submitForm.js

--- js/export-functions/submitForm.js
+++ js/export-functions/submitForm.js
@@ -1,16 +1,9 @@
 export async function submitForm(e, inputs, url) {
     e.preventDefault();
-    // let arrayOfInputValues = [];
-    // let arrayOfKeys = [];
     let dataObject = {};
     for (let i = 0; i < inputs.length; i++) {
         const singleInput = inputs[i];
-        // arrayOfKeys.push(singleInput.name);
-        // arrayOfInputValues.push(singleInput.value);
-        // dataObject[arrayOfKeys[i]] = arrayOfInputValues[i];
         dataObject[singleInput.name] = singleInput.value;
     }
-    // console.log("array of keys", arrayOfKeys);
-    // console.log("array of values ", arrayOfInputValues);
     console.log(dataObject);
 }
