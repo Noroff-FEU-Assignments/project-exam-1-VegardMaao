0d426985eab44e23dfa7382898459ef9b2b0fe8a
Vegard M <vm932648@gmail.com>
2023-04-15 14:32:08 +0200

    Reducing the function
    Shrank the function down into smaller pieces

--- js/export-functions/submitForm.js
+++ js/export-functions/submitForm.js
@@ -1,15 +1,18 @@
 export async function submitForm(e, inputs, url) {
     e.preventDefault();
-    let arrayOfInputValues = [];
-    let arrayOfKeys = [];
-    let dataObject = {};
+    // let arrayOfInputValues = [];
+    // let arrayOfKeys = [];
+    let dataObject = {
+        1: "asasdasdasd"
+    };
     for (let i = 0; i < inputs.length; i++) {
         const singleInput = inputs[i];
-        arrayOfKeys.push(singleInput.name);
-        arrayOfInputValues.push(singleInput.value);
-        dataObject[arrayOfKeys[i]] = arrayOfInputValues[i];
+        // arrayOfKeys.push(singleInput.name);
+        // arrayOfInputValues.push(singleInput.value);
+        // dataObject[arrayOfKeys[i]] = arrayOfInputValues[i];
+        dataObject[singleInput.name] = singleInput.value;
     }
-    console.log("array of keys", arrayOfKeys);
-    console.log("array of values ", arrayOfInputValues);
+    // console.log("array of keys", arrayOfKeys);
+    // console.log("array of values ", arrayOfInputValues);
     console.log(dataObject);
 }
