fdfa8cfa7b26e391bd5c2df470b14dcd6b030863
Vegard M <vm932648@gmail.com>
2023-04-27 15:04:03 +0200

    Update export-footer.js

--- js/export-objects/export-footer.js
+++ js/export-objects/export-footer.js
@@ -1,10 +1,12 @@
 // Footer items:
 export const footerItems = [{
+    title: "By Vegard Maaø",
+    url: "https://www.linkedin.com/in/vegard-m-a1bba7174/"
+},
+    {
     title: "About",
-    url: "about.html",
-    index: 0
+    url: "about.html"
 }, {
     title: "Contact",
-    url: "contact.html",
-    index: 1
+    url: "contact.html"
 }];
